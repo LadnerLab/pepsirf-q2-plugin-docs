c7fc00d855104b20d4c556947f4e6994559647cb
Annabelle Brown <amb2528@nau.edu>
2022-03-22 16:35:04 -0700

    88b5e66164bccf90a3be1c1b255b6128fe19f941

--- _static/usage.js
+++ _static/usage.js
@@ -1,24 +1,34 @@
-KNOWN_INTERFACES = {
-  'cli-usage': 'q2cli',
-  'artifact-usage': 'Python 3 API',
-};
-
 window.addEventListener('load', () => {
-  showInterface(Object.keys(KNOWN_INTERFACES)[0]);
+  const interfaceData = JSON.parse(document.getElementById('interfaces').innerHTML);
+  const KNOWN_INTERFACES = interfaceData['available'];
+
+  showInterface(interfaceData['default']);
 
   const interfaceSelect = document.createElement('select');
   interfaceSelect.className = 'usage-sync';
 
-  for (const [value, text] of Object.entries(KNOWN_INTERFACES)) {
+  const interfaceOptGroup = document.createElement('optgroup');
+  interfaceOptGroup.label = 'Interfaces';
+  interfaceSelect.appendChild(interfaceOptGroup);
+
+  const devOptGroup = document.createElement('optgroup');
+  devOptGroup.label = 'For Developers';
+  interfaceSelect.appendChild(devOptGroup);
+
+  for (const [value, [text, is_interface]] of Object.entries(KNOWN_INTERFACES)) {
     const opt = document.createElement('option');
     opt.value = value;
     opt.text = text;
-    interfaceSelect.appendChild(opt);
+    if (is_interface) {
+      interfaceOptGroup.appendChild(opt);
+    } else {
+      devOptGroup.appendChild(opt);
+    }
   };
 
   const anchorEls = document.querySelectorAll('span.usage-selector');
   if (anchorEls.length === 0) {
-    const usageEls = document.querySelectorAll('.cli-usage, .artifact-usage');
+    const usageEls = document.querySelectorAll(interfaceData['classes'].join(' '));
     if (usageEls.length > 0) {
       const errorMsg = 'must include at least one `.. usage-selector::` directive';
       document.body.innerHTML = errorMsg;
@@ -27,6 +37,7 @@ window.addEventListener('load', () => {
 
   anchorEls.forEach((el) => {
     const selectNode = interfaceSelect.cloneNode(true);
+    selectNode.value = interfaceData['default'];
     selectNode.addEventListener('change', (e) => showInterface(e.target.value));
     el.appendChild(selectNode);
   });
@@ -38,13 +49,13 @@ window.addEventListener('load', () => {
       selectEls.forEach((sel) => sel.selectedIndex = target.selectedIndex);
     }
   });
-});
 
-function showInterface(interfaceName) {
-  const knownInterfaceKeys = Object.keys(KNOWN_INTERFACES);
-  if (!knownInterfaceKeys.includes(interfaceName)) { return };
-  knownInterfaceKeys.forEach((el) => {
-    const hidden = interfaceName !== el;
-    document.querySelectorAll(`.${el}`).forEach((el) => el.hidden = hidden);
-  });
-}
+  function showInterface(interfaceName) {
+    const knownInterfaceKeys = Object.keys(KNOWN_INTERFACES);
+    if (!knownInterfaceKeys.includes(interfaceName)) { return };
+    knownInterfaceKeys.forEach((el) => {
+      const hidden = interfaceName !== el;
+      document.querySelectorAll(`.${el}`).forEach((el) => el.hidden = hidden);
+    });
+  }
+});
